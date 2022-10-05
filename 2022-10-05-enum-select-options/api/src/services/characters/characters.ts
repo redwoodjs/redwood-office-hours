65073b6a854c36cb67cfe8a1027417d596468c8a
David Thyresson <dthyresson@gmail.com>
2022-10-05 09:45:19 -0400

    Show error message if character already exists on CU

--- 2022-10-05-enum-select-options/api/src/services/characters/characters.ts
+++ 2022-10-05-enum-select-options/api/src/services/characters/characters.ts
@@ -1,6 +1,9 @@
 import type { QueryResolvers, MutationResolvers } from 'types/graphql'
+import { Prisma } from '@prisma/client'
+import { RedwoodError } from '@redwoodjs/api'
 
 import { db } from 'src/lib/db'
+import { logger } from 'src/lib/logger'
 
 export const characters: QueryResolvers['characters'] = () => {
   return db.character.findMany({ orderBy: { name: 'asc' } })
@@ -12,22 +15,50 @@ export const character: QueryResolvers['character'] = ({ id }) => {
   })
 }
 
-export const createCharacter: MutationResolvers['createCharacter'] = ({
+export const createCharacter: MutationResolvers['createCharacter'] = async ({
   input,
 }) => {
-  return db.character.create({
-    data: input,
-  })
+  try {
+    return await db.character.create({
+      data: input,
+    })
+  } catch (e) {
+    logger.error(e, 'Error creating character')
+
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // P2022: Unique constraint failed
+      // Prisma error codes: https://www.prisma.io/docs/reference/api-reference/error-reference#error-codes
+      if (e.code === 'P2002') {
+        logger.error('The character already exists', e)
+        throw new RedwoodError('The character already exists')
+      }
+    }
+    throw e
+  }
 }
 
-export const updateCharacter: MutationResolvers['updateCharacter'] = ({
+export const updateCharacter: MutationResolvers['updateCharacter'] = async ({
   id,
   input,
 }) => {
-  return db.character.update({
-    data: input,
-    where: { id },
-  })
+  try {
+    return await db.character.update({
+      data: input,
+      where: { id },
+    })
+  } catch (e) {
+    logger.error(e, 'Error updating character')
+
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // P2022: Unique constraint failed
+      // Prisma error codes: https://www.prisma.io/docs/reference/api-reference/error-reference#error-codes
+      if (e.code === 'P2002') {
+        logger.error('The character already exists', e)
+        throw new RedwoodError('The character already exists')
+      }
+    }
+    throw e
+  }
 }
 
 export const deleteCharacter: MutationResolvers['deleteCharacter'] = ({
